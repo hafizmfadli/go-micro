bc58ec16503a8f52f0839bf16a1f6738c01f06cb
hafizmfadli <lshafizmf1st@gmail.com>
2022-07-09 10:33:19 +0700

    broker - handle sending email

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -8,9 +8,17 @@ import (
 )
 
 type RequestPayload struct {
-	Action string `json:"action"`
-	Auth AuthPayload `json:"auth,omitempty"`
-	Log LogPayload `json:"log,omitempty"`
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
+	Mail   MailPayload `json:"mail,omitempty"`
+}
+
+type MailPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
 }
 
 type AuthPayload struct {
@@ -46,6 +54,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 	case "log":
 		app.logItem(w, requestPayload.Log)
+	case "mail":
+		app.sendMail(w, requestPayload.Mail)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
@@ -133,6 +143,36 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Error = false
 	payload.Message = "Authenticated!"
 	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) sendMail(w http.ResponseWriter, m MailPayload) {
+	jsonData, _ := json.MarshalIndent(m, "", "\t")
+
+	request, err := http.NewRequest("POST", "http://mail-service/send", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("send email failed"))
+		return
+	}
+
+	payload := jsonResponse{
+		Error: false,
+		Message: "Message sent to " + m.To,
+	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
